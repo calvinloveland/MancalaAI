Normalization, bug fixes

gym_mancala/envs/mancala_env.py
@@ -1,6 +1,7 @@
 import gym
 from gym import spaces
 from gym_mancala.envs.board import Board
+import numpy as np
 
 
 class MancalaEnv(gym.Env):
@@ -8,7 +9,7 @@ class MancalaEnv(gym.Env):
     def __init__(self):
         self.board = Board()
         self.action_space = spaces.Discrete(6)
-        self.observation_space = spaces.Box(low=0, high=46, shape=(2, 6))
+        self.observation_space = spaces.Box(low=-1, high=1, shape=(2, 6))
         self.player = 0
 
     def step(self, action):
@@ -16,11 +17,14 @@ class MancalaEnv(gym.Env):
 
     def reset(self):
         self.board = Board()
-        obs = self.board.marbles
+        obs = self.normalize_marbles()
         return obs
 
     def render(self, mode=None, close=None):
         self.board.print_board()
 
     def calculate_reward(self):
-        return self.board.mancala[0] - self.board.mancala[1] + sum(self.board.marbles[0]) - sum(self.board.marbles[1])
+        return self.board.mancala[self.player] - self.board.mancala[1 - self.player] + sum(self.board.marbles[self.player]) - sum(self.board.marbles[1 - self.player])
+
+    def normalize_marbles(self):
+        return np.divide(np.subtract(self.board.marbles, 3), 3)