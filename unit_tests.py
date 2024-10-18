ec79a0ce23ba7d076389f893914c69ce95e06375
Calvin Loveland <calvinloveland@gmail.com>
2024-10-18 17:56:59 -0600

    Add unit tests to prepare for switch away from keras
    Related to #1
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/calvinloveland/MancalaAI/issues/1?shareId=XXXX-XXXX-XXXX-XXXX).

--- unit_tests.py
+++ unit_tests.py
@@ -1,10 +1,15 @@
 import unittest
-
+import os
+import random
+import math
 import numpy as np
-
-from gym_mancala.envs import MancalaRandomEnv
+from keras.models import load_model
+from keras.optimizers import Adam
+from gym_mancala.envs import MancalaRandomEnv, MancalaUserEnv
 from gym_mancala.envs.board import Board
-
+from agent import build_agent
+from model import build_model
+from __main__ import train_network, test_networks, play_network
 
 class TestEnv(unittest.TestCase):
 
@@ -27,8 +32,38 @@ class TestEnv(unittest.TestCase):
         assert not random_env.board.player2_turn
         assert random_env.board.mancala[0] == 1
 
+    def test_build_model(self):
+        environment = MancalaRandomEnv()
+        model = build_model(environment)
+        assert model is not None
+        assert model.input_shape == (1,) + environment.observation_space.shape
+        assert model.output_shape == (None, environment.action_space.n)
 
+    def test_build_agent(self):
+        environment = MancalaRandomEnv()
+        model = build_model(environment)
+        agent = build_agent(model, environment, 1000)
+        assert agent is not None
+        assert agent.model == model
+        assert agent.nb_actions == environment.action_space.n
 
+    def test_train_network(self):
+        train_network()
+        assert os.path.exists('networks/Model10/model.HDF5')
+
+    def test_test_networks(self):
+        test_networks()
+        assert os.path.exists('networks/Model2/model.HDF5')
+
+    def test_play_network(self):
+        play_network()
+        model = load_model('networks/Model2/model.HDF5')
+        environment = MancalaUserEnv()
+        agent = build_agent(model, environment, 1000)
+        agent.compile(optimizer=Adam(lr=.01))
+        agent.load_weights('networks/Model2/4542')
+        history = agent.test(environment, nb_episodes=1, verbose=0)
+        assert history.history.get('episode_reward') is not None
 
 if __name__ == '__main__':
     print("Note:")
